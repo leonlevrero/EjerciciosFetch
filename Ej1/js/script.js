67dacf9737510f656aebe9c87f9ecfd7c466b314
leonlevrero <triplelevrero@gmail.com>
2022-08-17 17:24:42 -0300

    ejercicio fetch

--- Ej1/js/script.js
+++ Ej1/js/script.js
@@ -10,12 +10,18 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
-function showData(dataArray) {
+fetch(DATA_URL) // Se hace una petición al archivo JSON
+  .then(response => response.json()) // Se convierte el response en un objeto JSON
+  .then(data=> showData(data)) // Se llama a la función que muestra los datos en el DOM
+  .catch(error => console.log(error)); // Se captura el error en caso de que no se pueda obtener la información
+
+
+function showData(data) {
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
+  for (const item of data.students) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
     container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
-
+showData(); // Se llama a la función que muestra los datos en el DOM para que se muestren al cargar la página
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
--- Ej2/js/script.js
+++ Ej2/js/script.js
@@ -6,13 +6,19 @@ const DATA_URL = "https://dog.ceo/api/breeds/image/random"; // URL que contiene 
 const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
 const btnAddImage = document.getElementById("btnAddImage"); // "Traemos" utilizando el DOM el botón de id "btnAddImage" para asociarle un "escuchador de eventos" para el "click" del usuario
 
-/**
- * Función que recibe por parámetro un String con la URL de una imagen y
- * agrega esta imagen al div de id "container"
- */
+// Escribe el código necesario para añadir el evento click al botón que permita agregar perros al div de id "container"
+fetch(DATA_URL) // Se hace una petición al archivo JSON
+.then(response => response.json()) // Se convierte el response en un objeto JSON
+.then(data=> addImage(data.message)) // Se llama a la función que muestra los datos en el DOM
+.catch(error => console.log(error)); // Se captura el error en caso de que no se pueda obtener la información
+
+
 function addImage(imgURL) {
-  // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
+  btnAddImage.addEventListener("click", () => {
   container.innerHTML += `<img src="${imgURL}"/>`; // Se concatena cada imagen al innerHTML del contenedor
 }
+ );}
 
-// Escribe el código necesario para añadir el evento click al botón que permita agregar perros al div de id "container"
+
+
+addImage(); // Se llama a la función que muestra los datos en el DOM para que se muestren al cargar la página
