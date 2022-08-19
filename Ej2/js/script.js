2ef9c80a96b0b30bb5a1be330977d9a2ed7b6d5d
leonlevrero <triplelevrero@gmail.com>
2022-08-19 09:48:29 -0300

    Update script.js

--- Ej2/js/script.js
+++ Ej2/js/script.js
@@ -7,18 +7,19 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
 const btnAddImage = document.getElementById("btnAddImage"); // "Traemos" utilizando el DOM el botón de id "btnAddImage" para asociarle un "escuchador de eventos" para el "click" del usuario
 
 // Escribe el código necesario para añadir el evento click al botón que permita agregar perros al div de id "container"
+function nuevoperro() {
 fetch(DATA_URL) // Se hace una petición al archivo JSON
 .then(response => response.json()) // Se convierte el response en un objeto JSON
 .then(data=> addImage(data.message)) // Se llama a la función que muestra los datos en el DOM
 .catch(error => console.log(error)); // Se captura el error en caso de que no se pueda obtener la información
-
-
+ }
 function addImage(imgURL) {
-  btnAddImage.addEventListener("click", () => {
   container.innerHTML += `<img src="${imgURL}"/>`; // Se concatena cada imagen al innerHTML del contenedor
 }
- );}
+btnAddImage.addEventListener("click", () => {
+  nuevoperro(); // Se llama a la función que agrega un nuevo perro al DOM
+ }); // Se añade el evento "click" al botón para que se ejecute la función que muestra la imagen en el DOM
 
 
 
-addImage(); // Se llama a la función que muestra los datos en el DOM para que se muestren al cargar la página
+
